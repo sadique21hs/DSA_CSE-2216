a767eb8834b2f614ccac95e3545fba456c7a2c6d
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-07-07 22:41:25 +0600

    Update MaxMIn.c

---
+++ Linked_List/MaxMIn.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    node *start = NULL;
+    node *temp;
+    node *prev = NULL;
+    int ans, data;
+
+    printf("Do you want to add data (1/0): ");
+    scanf("%d", &ans);
+
+    while (ans == 1)
+    {
+        temp = malloc(sizeof(node));
+        printf("Enter data: ");
+        scanf("%d", &data);
+        temp->data = data;
+        temp->next = NULL;
+
+        if (start == NULL)
+        {
+            start = temp;
+        }
+        else
+        {
+            prev->next = temp;
+        }
+
+        prev = temp;
+        printf("Do you want to add more data (1/0): ");
+        scanf("%d", &ans);
+    }
+
+    printf("Displaying data: ");
+    display(start);
+    printf("\n");
+
+    printf("Displaying data in reverse order: ");
+    reverseDisplay(start);
+    printf("\n");
+
+    printf("Maximum value: %d\n", findMax(start));
+    printf("\n");
+    printf("Minimum value: %d\n", findMin(start));
+    printf("\n");
+    printf("Sum of all elements: %d\n", findSum(start));
+
+
+    return 0;
+}
