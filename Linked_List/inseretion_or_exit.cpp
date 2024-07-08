a16c40e6c7059a6f35323f25b5ad06ca3e5cb78c
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-07-09 01:15:52 +0600

    Update inseretion_or_exit.cpp

--- Linked_List/inseretion_or_exit.cpp
+++ Linked_List/inseretion_or_exit.cpp
@@ -6,3 +6,34 @@ struct list
 };
 typedef struct list node;
 node *start;
+
+
+
+
+int main()
+{
+    int choice,i=1,item;
+    start=NULL;
+    do
+    {
+        menu();
+        printf("Enter Choice: ");
+        scanf("%d",&choice);
+        if(choice==1)
+        {
+            printf("Enter item for insert: ");
+            scanf("%d",&item);
+            inseretion(item);
+            display();
+
+
+        }
+
+        else
+        {
+            i=0;
+        }
+
+    }
+    while(i);
+}
