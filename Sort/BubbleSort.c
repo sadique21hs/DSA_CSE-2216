d4e82ded2d003c512265cb3460ecb54c66879b0e
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-07-11 23:31:04 +0600

    Update BubbleSort.c

--- Sort/BubbleSort.c
+++ Sort/BubbleSort.c
@@ -18,4 +18,21 @@ int main()
         printf("%d ",arr[i]);
 
     }
+    for (i=0; i<n-1; i++)
+    {
+     
+        for( j=0; j<n-1; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+
+        }
+        
+
+
+    }
 }
