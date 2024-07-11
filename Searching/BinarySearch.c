3582e0bbf43736aedf1c50db3e53d0b55a7f53f1
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-07-12 02:03:07 +0600

    Update BinarySearch.c

--- Searching/BinarySearch.c
+++ Searching/BinarySearch.c
@@ -1,1 +1,35 @@
+int main()
+{
+    int arr[100],n,i,search,result ;
+    printf("Enter the array size: ");
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        printf("Enter the element: ");
+        scanf("%d",&arr[i]);
+    }for (i=0; i<n-1; i++)
+    {
+        int  flag=1; //for better
+        for( j=0; j<n-1; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
 
+        }
+        if(flag==0)
+        {
+            break;
+        }
+
+
+    }
+    printf("\nAfter sort:\n");
+
+    for( i=0; i<n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
