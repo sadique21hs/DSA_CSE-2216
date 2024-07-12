85e22fa1fe413eb2929e7258168790418f38fc04
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-07-12 23:14:59 +0600

    Update Inseretion_or_delete.cpp

--- Linked_List/Inseretion_or_delete.cpp
+++ Linked_List/Inseretion_or_delete.cpp
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct list
+{
+    int data;
+    struct list *next;
+};
+typedef struct list node;
+
+node *start = NULL;
+
+void display()
+{
+    node *temp = start;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int choice, item;
+
+    do
+    {
+        menu();
+        printf("Enter Choice: ");
+        scanf("%d", &choice);
+        display();
+    }
+      }
