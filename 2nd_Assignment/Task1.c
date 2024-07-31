f5e6bdf381ea361880212c005093fa2ecc9c0644
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-07-31 15:23:52 +0600

    Update Task1.c

--- 2nd_Assignment/Task1.c
+++ 2nd_Assignment/Task1.c
@@ -1,1 +1,19 @@
 
+typedef struct List
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    struct Node *top;
+    struct Node *bottom;
+} ;
+typedef struct List Node;
+
+  Node  *createNode(int data)
+{
+    Node  *newNode;
+    newNode = malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = newNode->top = newNode->bottom = NULL;
+    return newNode;
+}
